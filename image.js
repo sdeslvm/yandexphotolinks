e5fbc6135d15ef9ac872c6095a0419b714ebb085
uqukina <a1@MacBook-Air-1.local>
2025-04-09 00:22:42 +0300

    commit changes25

--- image.js
+++ image.js
@@ -1,35 +1,20 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 module.exports = async (req, res) => {
-    const { url } = req.query;
-    
-    if (!url) {
-        return res.status(400).send('No URL provided');
-    }
-
     try {
-        const imageRes = await fetch(url, {
+        const response = await axios({
+            method: 'get',
+            url: req.query.url,
+            responseType: 'arraybuffer',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
-                'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-                'Referer': 'https://www.dekomo.ru/',
-                'Origin': 'https://www.dekomo.ru'
+                'Referer': 'https://www.dekomo.ru'
             }
         });
 
-        if (!imageRes.ok) {
-            throw new Error(`Image fetch failed with status ${imageRes.status}`);
-        }
-
-        const contentType = imageRes.headers.get('content-type');
-        const arrayBuffer = await imageRes.arrayBuffer();
-        
-        res.setHeader('Content-Type', contentType || 'image/jpeg');
-        res.send(Buffer.from(arrayBuffer));
-        
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(response.data);
     } catch (error) {
-        console.error('Error:', error.message);
+        console.error(error);
         res.status(500).send(error.message);
     }
 }; 
